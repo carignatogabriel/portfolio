404974dd634cbf46b414c67473c32365ab46a065
GabrielCarignato <109916984+carignatogabriel@users.noreply.github.com>
2023-03-08 20:04:01 -0300

    Fim-do-projeto
    Projeto finalizado.

---
+++ script.js
@@ -0,0 +1,29 @@
+
+let sliderElement = document.querySelector('#slider');
+let buttonElement = document.querySelector('#button');
+
+let sizePassword = document.querySelector('#valor');
+let password = document.querySelector('#password');
+
+let containerpassword = document.querySelector('#container-password');
+
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+let novasenha = ""
+
+sizePassword.innerHTML =  sliderElement.value;
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value
+}
+
+function generatepassword() {
+    let pass = "";
+    for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+        pass += charset.charAt(Math.floor(Math.random() * n));
+        
+    }
+    containerpassword.classList.remove('hide')
+    password.innerHTML = pass;
+    novasenha = pass
+}
+
